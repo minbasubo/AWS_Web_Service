40a24f70904d8e78385a7f4db3d793a8155f51c6
minbasubo <suwh2600@naver.com>
2022-08-04 15:41:53 +0900

    Make Controller and Test case with lombok

---
+++ src/main/java/com/AWS_Web_Service/admin/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.AWS_Web_Service.admin.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+}
+
---
+++ src/test/java/com/AWS_Web_Service/admin/dto/HelloResponseDtoTest.java
@@ -0,0 +1,29 @@
+package com.AWS_Web_Service.admin.dto;
+
+import com.AWS_Web_Service.admin.web.dto.HelloResponseDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDtoTest {
+
+    @Test
+    public void LombokFuncTest() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
+
